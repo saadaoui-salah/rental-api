5e0d32129cd7c25aaa17e2a8a4c35a580367370a
saadaoui-salah <salahsaadaoui8@gmail.com>
2024-06-13 15:52:32 -0400

    add: listing model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import get_user_model
+import rest_framework
+from users.models import CustomUsCustomUser
+User = get_user_model()
+
+
+class DetailsSection(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
 
 # Create your models here.
+class Listing(models.Model):
+    PENDING = "PENDING"
+    ACTIVE = "ACTIVE"
+    RENT = "RENT"
+
+    status_choices = [
+        (PENDING, "Pending"),
+        (ACTIVE, "Active"),
+        (RENT, "Rent"),
+        ]
+
+    user = models.ForeignKey(User,limit_choices_to={'user_type':CustomUser.SELLER}, on_delete=models.CASCADE)
+    price = models.FloatField(max_length=100)
+    status = models.CharField(choices=status_choices)
+    address = models.CharField(max_length=200)
+    unit_number = models.CharField(max_length=200)
